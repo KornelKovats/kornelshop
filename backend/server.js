9bdb0c57ea481bc9a8382dfd0a33330aa9f8d72f
KornelKovats <kovats.kornel95@gmail.com>
2021-01-06 20:45:07 +0100

    backend server up

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import data from './data.js'
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/api/products',(req,res) => {
+    res.send(data.products);
+})
+app.get('/', (req, res)=>{
+    res.send('Server is ready');
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Serve at http://localhost:${PORT}`);
+    
+})
