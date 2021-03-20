9fac9538a4bf7693bde98ce74ece88e13179f2f2
Kovats Kornel <kornel.kovats@idomsoft.hu>
2021-03-20 21:16:04 +0100

    added store to react app

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,17 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import Product from "../components/Product";
 import MessageBox from "../components/MessageBox";
 import LoadingBox from "../components/LoadingBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProduct } from "../actions/productActions";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList;
+  useEffect(() => { 
+    dispatch(listProduct())
+  }, [dispatch]); 
   return (
     <div>
       {loading ? (
